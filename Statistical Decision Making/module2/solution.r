4db6eed5a5bb3187a7f0fa7a691c58fbf71ebf20
Chea N.Krouch <Krouchcheangoun@gmail.com>
2023-06-12 11:46:28 +1000

    module 2 complete

--- Statistical Decision Making/module2/solution.r
+++ Statistical Decision Making/module2/solution.r
@@ -1,1 +1,16 @@
+#1 Load ExerciseHours
+library(Lock5Data)
+data("ExerciseHours")
+attach(ExerciseHours)
+
+#2 Plot relationship between Sex/Hand, Sex/Pulse, Exercise/Pulse
+# Sex/Hand, they are both categorical variable so we will use barplot
+barplot(table(Sex, Hand), legend=TRUE) #it seem that men are more likely to be left-handed
+
+# Sex/pulse, Sex is categorical and Pulse is quantitative so we use boxplot
+boxplot(Pulse ~ Sex) #it seem that female have higher range of pulses
+
+#Exercise/Pulse Both are quantitative so we use plot
+plot(Exercise,Pulse) #the plot doesn't show any strong association between the two
+cor(Exercise,Pulse) #the correlation is small indicating a weak linear association
 
